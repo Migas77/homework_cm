a02367ecef4a44bbf7411fae1608c6afadf48a70
Migas77 <miguel.belchior@ua.pt>
2024-10-04 15:39:54 +0100

    Glória, Aveiro

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:too_good_to_go_clone/mycolors/colors.dart';
 import 'package:too_good_to_go_clone/providers/stock_stores_state.dart';
 import 'package:too_good_to_go_clone/widgets/small_store_card.dart';
 
@@ -19,7 +20,51 @@ class HomeScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             children: [
-              const Text('Glória, Aveiro'),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Column(
+                      children: [
+                        SizedBox(height: 5),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.location_on_sharp, size: 14, color: MyColorPalette.darkGreen)
+                        )
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: const TextSpan(
+                            children: [
+                              TextSpan(text: "Glória, Aveiro", style: TextStyle(
+                                color: MyColorPalette.darkGreen,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                              )),
+                              WidgetSpan(
+                                  alignment: PlaceholderAlignment.middle,
+                                  child: Icon(Icons.keyboard_arrow_down_outlined, size: 25, color: MyColorPalette.darkGreen)
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text("dentro de 10 km", style: TextStyle(
+                          color: MyColorPalette.darkGreen,
+                          fontSize: 14.0,
+                        )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+
+
               const SearchBar(
                   padding: WidgetStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(horizontal: 16.0)),
                   leading: Icon(Icons.search),
