691e5ddf34ca11e27b0233b9762937ad7ec8c6d3
Migas77 <miguel.belchior@ua.pt>
2024-10-08 10:39:43 +0100

    animated switcher empty favorite screen

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -25,51 +25,57 @@ class FavoritesScreen extends StatelessWidget {
             )),
           ),
         ),
-        if (favoritesState.favoriteStores.isEmpty)
-          Expanded(child:
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 50),
-                  const Text("Ainda não tens favoritos", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22
-                  )),
-                  const SizedBox(height: 4),
-                  const SizedBox(
-                    width: 300,
-                    child: Text("Clica no ícone do coração numa loja para adicioná-la aos teus favoritos e ela aparecerá aqui.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: 15.5
-                      ),
-                    )
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MyColorPalette.darkGreen,
-                      minimumSize: const Size(200, 0),
-                    ),
-                    onPressed: () => onButtonPress(),
-                    child: const SizedBox(
-                      width: 100,
-                      child: Text("Encontrar uma Surprise Bag",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          height: 1.6,
-                          color: Colors.white,
+        Expanded(
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: favoritesState.favoriteStores.isEmpty
+              ? Column(
+                  key: const ValueKey("empty"),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50),
+                    const Text("Ainda não tens favoritos", style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22
+                    )),
+                    const SizedBox(height: 4),
+                    const SizedBox(
+                        width: 300,
+                        child: Text("Clica no ícone do coração numa loja para adicioná-la aos teus favoritos e ela aparecerá aqui.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 15.5
+                          ),
                         )
-                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: MyColorPalette.darkGreen,
+                          minimumSize: const Size(200, 0),
+                        ),
+                        onPressed: () => onButtonPress(),
+                        child: const SizedBox(
+                          width: 100,
+                          child: Text("Encontrar uma Surprise Bag",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                height: 1.6,
+                                color: Colors.white,
+                              )
+                          ),
+                        )
                     )
-                  )
-                ],
-              )
-
-          ) else
-            Expanded(child: StoresList(stores: favoritesState.favoriteStores))
+                  ],
+                )
+              : StoresList(
+                    key: const ValueKey("notEmpty"),
+                    stores: favoritesState.favoriteStores
+                )
+          )
+        )
       ]
 
     );
